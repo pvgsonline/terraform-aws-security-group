5dbd6960b7675aa243cb2fab100cb2e4cbd7d528
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-10-01 13:11:30 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  sg_name = "${var.project_name}-${var.environment}-${var.security_group_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "security_group_name" {
+
+}
+
+variable "common_tags"{
+    default = {}
+}
+
+variable "vpc_id"{
+
+}
